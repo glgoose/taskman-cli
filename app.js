5ec90c73e3a8f011df3c4141222f06a77e6ee5fb
Glenn Goossens <glgoose@pm.me>
2021-05-18 10:23:01 +0200

    adds list command

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let argv = require('yargs/yargs')(process.argv.slice(2)).argv
 
 const tasks = ['task1', 'task2']
 
-if (argv.ls) {
+if ((argv._[0] === 'ls') | (argv[0] === 'list')) {
   console.log(tasks)
 }
