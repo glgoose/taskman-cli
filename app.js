3b7bcec06f173833e462a91a6b805e96d1df6d46
Glenn Goossens <glgoose@pm.me>
2021-05-18 10:36:02 +0200

    add & save tasks

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-let argv = require('yargs/yargs')(process.argv.slice(2)).argv
+let argv = require('yargs/yargs')(process.argv.slice(2)).alias('a', 'add').argv
+const fs = require('fs')
 
 const tasks = ['task1', 'task2']
 
 if (argv._[0] === 'ls' || argv._[0] === 'list') {
   console.log(tasks.join('\n'))
 }
+
+if (argv.a || argv.add) {
+  tasks.push(argv.a)
+}
+
+const saveTasks = tasks => {
+  try {
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+saveTasks(tasks)
