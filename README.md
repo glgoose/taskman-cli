9fc6a5fdb414ac6638db9fa82a48db787ebcbfc2
Glenn Goossens <glgoose@pm.me>
2021-05-18 13:58:13 +0200

    adds usage examples

---
+++ README.md
@@ -0,0 +1,16 @@
+# Task manager
+
+## Usage
+
+Example:
+
+```shell
+node app ls             # lists all tasks & shows index
+node app -a 'buy food'  # adds task 'buy food'
+node app rm 2           # removes task with index 2
+node app -c 2           # marks task with index 2 as completed
+```
+
+## Installation
+
+`npm install`
--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let argv = require('yargs/yargs')(process.argv.slice(2))
   .alias('a', 'add')
-  .alias('c', 'complete').argv
+  .alias('c', 'complete')
+  .alias('h', 'help')
+  .help('h')
+  .example('node app ls', 'lists all tasks')
+  .example("node app -a 'buy food'", "adds task 'buy food'")
+  .example('node app rm 2', 'removes task with index 2')
+  .example('node app -c 2', 'marks task with index 2 as completed').argv
 const fs = require('fs')
 const chalk = require('chalk')
 
