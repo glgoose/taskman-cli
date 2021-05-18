6735ce7f230ec5dde8a492dbd6d65a4738d57959
Glenn Goossens <glgoose@pm.me>
2021-05-18 10:25:24 +0200

    fix second part of ls conditional

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let argv = require('yargs/yargs')(process.argv.slice(2)).argv
 
 const tasks = ['task1', 'task2']
 
-if ((argv._[0] === 'ls') | (argv[0] === 'list')) {
+if (argv._[0] === 'ls' || argv._[0] === 'list') {
   console.log(tasks.join('\n'))
 }
