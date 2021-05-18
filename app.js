dc8f3ecb101312e7bf3aebf5035090ecea49a563
Glenn Goossens <glgoose@pm.me>
2021-05-18 10:24:00 +0200

    print tasks 1 per line

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ let argv = require('yargs/yargs')(process.argv.slice(2)).argv
 const tasks = ['task1', 'task2']
 
 if ((argv._[0] === 'ls') | (argv[0] === 'list')) {
-  console.log(tasks)
+  console.log(tasks.join('\n'))
 }
