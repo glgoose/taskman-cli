705272431ad7a0a4e9c19be933715ae4522b96ab
Glenn Goossens <glgoose@pm.me>
2021-05-18 12:59:21 +0200

    refractor remove action & rm brackets from 1 line if statements

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let argv = require('yargs/yargs')(process.argv.slice(2))
   .alias('a', 'add')
-  .alias('rm', 'remove')
-  .alias('c', 'complete', 'done').argv
+  .alias('c', 'complete').argv
 const fs = require('fs')
-const { access } = require('node:fs')
 
 const filePath = 'tasks.json'
 
+// load/read existing tasks
 const loadTasks = () => {
   try {
     const file = fs.readFileSync(filePath, 'utf8')
@@ -20,26 +19,22 @@ let tasks = loadTasks()
 if (!tasks) tasks = []
 
 // command arguments
-if (argv._[0] === 'ls' || argv._[0] === 'list') {
+if (argv._[0] === 'ls' || argv._[0] === 'list')
   tasks.forEach((task, idx) => console.log(`${idx} | ${task.name}`))
-}
 
-if (argv.add) {
-  tasks.push({ name: argv.add, done: false })
-}
+if (argv.add) tasks.push({ name: argv.add, done: false })
 
-if (argv.delete) {
-  tasks.splice(argv.delete, 1)
-}
+if (argv._[0] === 'rm' || argv._[0] === 'remove') tasks.splice(argv._[1], 1)
 
-if (argv.complete)
-  // store tasks
-  const storeTasks = tasks => {
-    try {
-      fs.writeFileSync(filePath, JSON.stringify(tasks))
-    } catch (err) {
-      console.log(err)
-    }
+if (argv.done) tasks[argv.done].done = true
+
+// store tasks
+const storeTasks = tasks => {
+  try {
+    fs.writeFileSync(filePath, JSON.stringify(tasks))
+  } catch (err) {
+    console.log(err)
   }
+}
 
 storeTasks(tasks)
